0530e022a1dd766b1e3a8b43ff21e80d35754cce
m-blank-a <emagsayo4138@gmail.com>
2025-12-21 17:39:46 +0800

    4th try I swear if this doesn't work

--- PetGrooming/res/RemoteObject.js
+++ PetGrooming/res/RemoteObject.js
@@ -1,8 +1,31 @@
 "use strict";
+
 class RemoteObject {
-    Request() {
-        let data = {
+    data = {};
+    Request(link, obj) {
+        if (typeof link !== "string")
+            return; // link must be a string
+        if (obj == null || typeof obj === "undefined")
+            return; // Ignore null
+        if (typeof obj !== "object" || obj.constructor !== Object)
+            return; // Only accept plain JS objects, not from classes.
+        if (Object.keys(obj).length == 0)
+            return; // Ignore blank requests
+
+        let url = new URL(link);
+        let requestToSend = {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        }
+        
+        let isRequestSuccess = false;
+        do {
+            let promise = fetch(url, requestToSend);
             
-        };
+        } while (!isRequestSuccess);
     }
-}
+} 
